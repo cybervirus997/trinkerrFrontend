a23129accc48195e23f2bc34fdb3fc39a596037b
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-12-18 17:05:15 +0530

    60% done

--- src/Components/CompanyData.jsx
+++ src/Components/CompanyData.jsx
@@ -8,13 +8,19 @@ export const CompanyData = () => {
 
     useEffect(() => {
         setData(TrinkerrData);
+        StoreDataLocalStroge();
     }, [])
+
+    const StoreDataLocalStroge = () => { 
+        let newArr = localStorage.getItem("stock");
+        console.log(newArr);
+    }
 
     return (
         <div className="col-12" style={{ borderBottom: "3px solid #F2F2F2" }}>
             
             {
-                data.map((el) => {
+                data.map((el,i) => {
                     let UnsplitedName = el[0].split("::");
                     let name = UnsplitedName[0];
                     let nesdek = UnsplitedName[1];
@@ -22,10 +28,10 @@ export const CompanyData = () => {
                     let per = (((Number(el[1]) - Number(el[2])) / Number(el[2])) * 100).toFixed(2);
 
                     return (
-                    <CompanyName className = "p-4 pb-2" >
+                        <CompanyName className="p-4 pb-2" key={i} >
                             <div className = "col-3">
                                 {
-                                    el[1] > el[2] ?
+                                    el[1] < el[2] ?
                                         <p style={{color:"#E7592E" }}>{name}</p> :
                                         <p style={{color:"#29C5C1" }}>{name}</p> 
                                 }
@@ -34,7 +40,7 @@ export const CompanyData = () => {
                         </div>
                             <div className ="col-2">
                                 {
-                                    el[1] > el[2] ?
+                                    el[1] < el[2] ?
                                         <p style={{ textAlign: "end",color:"#E7592E" }}>{curr}</p> :
                                         <p style={{ textAlign: "end",color:"#29C5C1" }}>{curr}</p>   
                                 }
@@ -42,7 +48,7 @@ export const CompanyData = () => {
                             <p style={{textAlign: "end",fontSize:"20px"}}>
                                     <span style={{marginRight:"10px"}}>
                                         {
-                                            el[1] > el[2] ?
+                                            el[1] < el[2] ?
                                                 <img src="https://user-images.githubusercontent.com/72969348/146635175-7fc168e9-2372-4596-9085-f231a0cbd3bd.png" alt="down" /> :
                                                 <img src="https://user-images.githubusercontent.com/72969348/146636189-e3bbb9b2-15a3-4c10-91b2-2f79a5dddf4a.png" alt="upper" />
                                         }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,10 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Appbar } from './CssStore'
+import { TrinkerrData } from '../Maindata'
+import { CompanyName } from './CssStore'
 
 export const SearchBar = () => {
+
+    const [title, setTitle] = useState("")
+    const [wholeData,setWholeData]=useState([])
+
+    useEffect(() => { 
+        searchTypeData();
+        // console.log(wholeData);
+    }, [title])
+    
+    
+    const searchTypeData = () => { 
+        let arr = [];
+        TrinkerrData.map((el,i) => { 
+                let UnsplitedName = el[0].split("::");
+                let name = UnsplitedName[0];
+            if (title.length === 0)
+            { 
+                arr=[]
+            }
+            
+            if (name.includes(title.toUpperCase()) && title.length!==0)
+            { 
+                arr=[...arr,el]
+            }
+            
+        })
+        setWholeData(arr);
+    }
+
     return (
-        <div className="col-12 p-4">
-            <Appbar type="type" name="searchbar" placeholder="Search stocks..." />
-        </div>
+        <>
+            <div className="col-12 p-4">
+                <Appbar type="type" name="searchbar" placeholder="Search stocks..." onChange={(e)=>setTitle(e.target.value)} />
+            </div>
+
+            <div className='col-8' style={{position: "absolute",background:"white",top:"99px"}}>
+                {/* sdfgh */}
+                    {
+                    wholeData.map((el,i) => {
+                        let UnsplitedName = el[0].split("::");
+                        let name = UnsplitedName[0];
+                        let nesdek = UnsplitedName[1];
+                        let curr = Number(el[2]);
+                        let per = (((Number(el[1]) - Number(el[2])) / Number(el[2])) * 100).toFixed(2);
+
+                        return (
+                            <CompanyName className="p-4 pb-2" key={i} style={{borderBottom: "3px solid #F2F2F2"}} >
+                                <div className = "col-3">
+                                    {
+                                        el[1] > el[2] ?
+                                            <p style={{color:"#E7592E" }}>{name}</p> :
+                                            <p style={{color:"#29C5C1" }}>{name}</p> 
+                                    }
+                                    
+                                    <p style={{color:"#9D9D9D",fontSize:"20px"}}>{nesdek}</p>
+                                </div>
+                                
+                                <div>
+                                    <button onClick={()=>console.log("clickedData",el)}>click</button>
+                                </div>
+
+                                <div className ="col-2">
+                                    {
+                                        el[1] < el[2] ?
+                                            <p style={{ textAlign: "end",color:"#E7592E" }}>{curr}</p> :
+                                            <p style={{ textAlign: "end",color:"#29C5C1" }}>{curr}</p>   
+                                    }
+                                    
+                                <p style={{textAlign: "end",fontSize:"20px"}}>
+                                        <span style={{marginRight:"10px"}}>
+                                            {
+                                                el[1] < el[2] ?
+                                                    <img src="https://user-images.githubusercontent.com/72969348/146635175-7fc168e9-2372-4596-9085-f231a0cbd3bd.png" alt="down" /> :
+                                                    <img src="https://user-images.githubusercontent.com/72969348/146636189-e3bbb9b2-15a3-4c10-91b2-2f79a5dddf4a.png" alt="upper" />
+                                            }
+                                        </span>
+                                        {per}%
+                                </p>
+                            </div>
+                        </CompanyName>
+                        )
+                        
+                    }) 
+                }
+                {/* dfghj */}
+            </div>
+
+        </>    
     )
 }
